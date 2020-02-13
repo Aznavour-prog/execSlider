4b9d6b6dde7fdb0e18398c0839c2d8fae3661f91
Aznavour <vic007tor@gmail.com>
2020-02-13 15:19:18 -0500

    ajout d'une autre branche

--- js/generationElement.js
+++ js/generationElement.js
@@ -1,11 +1,15 @@
 function genereSlider(){
+    var tableauImage = ["1.jpeg", "2.jpg","3.jpg","4.jpg","5.jpg"];
     var boutonsSlider = document.querySelectorAll(".boutonSlider");
     let sectionSlider = document.querySelector(".slides");
     console.log(boutonsSlider);
     for(let i = 0;i<boutonsSlider.length;i++){
         console.log(boutonsSlider[i]);
-        let imageSlider = document.createElement("div");
-        imageSlider.id = "slide-" + (i + 1);
-        sectionSlider.appendChild(imageSlider);
+        let divSlider = document.createElement("div");
+        let imageSlider = document.createElement("img");
+        imageSlider.src ="../img/"+ tableauImage[i];
+        divSlider.appendChild(imageSlider);
+        divSlider.id = "slide-" + (i + 1);
+        sectionSlider.appendChild(divSlider);
     }
 }
